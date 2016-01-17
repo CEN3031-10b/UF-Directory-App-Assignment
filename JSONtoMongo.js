ed91ea343b42be4178738165ec270063a2dce88e
Michael Rodriguez <michaelrod@gmail.com>
2016-01-17 13:46:54 -0500

    previous commit empty, added files

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,14 +10,43 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
+mongoose.connect(config.db.uri);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
 
+// read listings.json
+var listings = JSON.parse(fs.readFileSync('listings.json', 'utf8')).entries;
+
+// go through listings.json and add entries to the db
+// use callback counter disconnect from the db when complete
+var entries_counter = 0;
+var callback = function(err){
+	if(err){
+		throw err;
+	}
+	entries_counter++;
+	if(entries_counter == (listings.length)){
+		console.log(listings.length + " listing entries added to mongolab.")
+		mongoose.disconnect();
+	} 
+}
+
+for(var i = 0; i < listings.length; ++i){
+	new Listing(listings[i])
+	.save(callback);
+}
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
  */
+
+// test print out of all listings (comment out insert code)
+// Listing.find({}, function(err, l) {
+//  if (err) throw err;
+//  console.log(l);
+//});
+
--- ListingSchema.js
+++ ListingSchema.js
@@ -4,13 +4,41 @@ var mongoose = require('mongoose'),
 
 /* Create your schema */
 var listingSchema = new Schema({
-  /* your code here */
+	code: {
+        type:String,
+        required: true,
+        unique: true
+    },
+	name: {
+        type:String,
+        required: true,
+    },
+	coordinates: {
+		lat: Number,
+		lng: Number
+	},
+	address: String,
+	updated_at: Date,
+	created_at: Date
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
-  /* your code here */
+	
+	if(!this.code && !this.name){
+		var err = new Error('Listing save failed: Missing required field(s)[code,name]');
+		next(err);
+	}
+	
+	if(!this.created_at){
+		this.created_at = Date.now;
+	}
+	
+	this.updated_at = Date.now;
+	
+	next();
 });
+
 
 /* Use your schema to instantiate a Mongoose model */
 var Listing = mongoose.model('Listing', listingSchema);
